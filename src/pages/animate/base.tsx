433cdf231ff8fca1c10679d2e7b5689c10b6947b
云墨 <lg208143@alibaba-inc.com>
2021-06-05 15:51:04 +0800

    feat: 添加方块

--- src/pages/animate/base.tsx
+++ src/pages/animate/base.tsx
@@ -1,8 +1,9 @@
 import {createElement} from 'react';
 import * as THREE from 'three';
 import Base from '@/component/base/index';
+import { Projector } from 'three/examples/jsm/renderers/Projector';
 
-export class Component extends Base {
+export default class Component extends Base {
   scene = new THREE.Scene();
   renderer = new THREE.WebGL1Renderer({
     antialias: true,
@@ -15,6 +16,34 @@ export class Component extends Base {
   }
 
   init = () => {
+    this.addCube();
+    // 设计相机
+    this.camera.position.x = 100;
+    this.camera.position.y = 100;
+    this.camera.position.z = 100;
+    this.camera.lookAt(this.scene.position);
+    this.scene.add(this.camera);
+
+    // 添加光源
+    const ambientLight = new THREE.AmbientLight(0x0c0c0c);
+    this.scene.add(ambientLight);
+    const spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(100, 100, 100);
+    spotLight.castShadow = true;
+    this.scene.add(spotLight);
+
+    this.renderer.setClearColor(new THREE.Color(0xeeeeee));
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.renderCanvas();
+  }
+
+  addCube = () => {
+    const cubeGeometry = new THREE.BoxGeometry(20,20, 20);
+    const mesh = new THREE.Mesh(cubeGeometry, new THREE.MeshLambertMaterial({
+      color: 0xffffff
+    }));
+    mesh.castShadow = true;
+    this.scene.add(mesh);
   }
 
   renderCanvas = () => {
